3183da97bb54d0c2309146a0bc28d3d57bebc72e
Brian Cardarella <bcardarella@gmail.com>
2010-07-06 13:19:59 -0400

    Updated to client_side_validations 2.1.0

--- rails_3/config/application.rb
+++ rails_3/config/application.rb
@@ -39,6 +39,8 @@ module Rails3
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+    
+    config.middleware.use 'ClientSideValidations'
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
