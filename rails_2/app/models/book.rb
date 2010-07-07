63363e19316413bdb3e4b82b287d443c39828123
Brian Cardarella <bcardarella@gmail.com>
2010-07-07 19:51:09 -0400

    Updated to client_side_validations 2.2.2

--- rails_2/app/models/book.rb
+++ rails_2/app/models/book.rb
@@ -1,9 +1,10 @@
 class Book < ActiveRecord::Base
 
-  validates_presence_of :author
-  validates_length_of :author, :minimum => 10, :message => "That is waaaay too short!"
+  validates_presence_of     :author
+  validates_length_of       :author, :minimum => 10, :message => "That is waaaay too short!"
+  validates_uniqueness_of   :author
   
-  validates_presence_of :volume
+  validates_presence_of     :volume
   validates_numericality_of :volume
 
 end
