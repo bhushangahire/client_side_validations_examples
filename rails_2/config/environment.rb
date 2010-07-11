0e27bf7d309c9827cc40a2a5417cc43c102848c7
Brian Cardarella <bcardarella@gmail.com>
2010-07-11 10:42:11 -0400

    Upgraded to client_side_validations 2.4.1

--- rails_2/config/environment.rb
+++ rails_2/config/environment.rb
@@ -7,7 +7,7 @@ RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'client_side_validations', :version => '2.3.0'
+  config.gem 'client_side_validations', :version => '2.4.1'
   config.time_zone = 'UTC'
   config.middleware.use 'ClientSideValidations::Uniqueness'
 end
