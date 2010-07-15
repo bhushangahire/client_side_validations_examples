96d55820a3dceb44d25bfd444cb7c352e418cf7d
Brian Cardarella <bcardarella@gmail.com>
2010-07-15 15:54:42 -0400

    Updated to client_side_validations 2.6.1
    Also ignored Gemfile.lock in Rails 3 app

--- rails_3/app/models/book.rb
+++ rails_3/app/models/book.rb
@@ -4,7 +4,6 @@ class Book < ActiveRecord::Base
   validates_length_of       :author, :minimum => 10, :message => "That is waaaay too short!", :on => :update
   validates_uniqueness_of   :author
   
-  validates_presence_of     :volume
   validates_numericality_of :volume, :on => :create
 
 end
